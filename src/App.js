a932f3b92b54b9dd8c8569dfbd5898d205167dd6
excavator <str_token@mail.ru>
2019-12-16 13:24:19 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 0
+  }
+
+  updateCounter(value) {
+    this.setState({
+      counter: this.state.counter + value
+    })
+  }
+
+  render() {
+    return (
+      <div className={'App'}>
+        <h1>Счетчик <strong>{this.state.counter}</strong></h1>
+
+        <hr/>
+
+        <div className="Actions">
+          <button onClick={() => this.updateCounter(1)}>Добавить 1</button>
+          <button onClick={() => this.updateCounter(-1)}>Вычесть 1</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
