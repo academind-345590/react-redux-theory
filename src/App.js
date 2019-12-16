8d8ffac33e06ddc57fd60ae7f67ece5881a18e60
excavator <str_token@mail.ru>
2019-12-16 18:10:01 +0300

    transfer params

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.scss'
 class App extends Component {
   
   render() {
-    console.log(this.props);
+    //console.log(this.props);
     return (
       <div className={'App'}>
         <h1>Счетчик <strong>{this.props.counter}</strong></h1>
@@ -15,6 +15,10 @@ class App extends Component {
         <div className="Actions">
           <button onClick={this.props.onAdd}>Добавить 1</button>
           <button onClick={this.props.onSub}>Вычесть 1</button>
+        </div>
+        <div className="Actions">
+          <button onClick={()=>this.props.onAddNumber(15)}>Добавить 15</button>
+          <button onClick={()=>this.props.onSubNumber(17)}>Вычесть 17</button>
         </div>
       </div>
     )
@@ -30,7 +34,9 @@ function mapStateToProps(state){
 function mapDispatchToProps(dispatch){
   return {
     onAdd: ()=> dispatch({type: 'ADD'}),
-    onSub: ()=> dispatch({type: 'SUB'})
+    onSub: ()=> dispatch({type: 'SUB'}),
+    onAddNumber: number => dispatch({type: 'ADD_NUMBER', payload: number}),
+    onSubNumber: number => dispatch({type: 'SUB_NUMBER', payload: number})
   }
 }
 
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -13,7 +13,15 @@ export default function rootReducer(state=initialState, action){
       return {
         counter: state.counter - 1
       }
+    case 'ADD_NUMBER':
+      return{
+        counter: state.counter + action.payload
+      }
+    case 'SUB_NUMBER':
+      return{
+        counter: state.counter - action.payload
+      }
+    default:
+      return state
   }
-
-  return state
 }
